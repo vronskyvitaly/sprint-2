dcf13df8d9b1d6fb0dd05e79ad70fc378cf38c06
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2023-11-02 18:19:17 +0300

    done task HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const sortedState = [...state];
+            if (action.payload === "up") {
+                sortedState.sort((a,b)=> a.name.localeCompare(b.name) )
+            }
+            else if (action.payload === "down") {
+                sortedState.sort((a, b) => b.name.localeCompare(a.name));
+            }
+            return sortedState
 
-            return state // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el=> el.age >= action.payload)
         }
         default:
             return state
